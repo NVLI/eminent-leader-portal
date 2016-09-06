c192de99322ad451f858aa386eb87c7fe80c149c
magi yv <magi.yv@zyxware.com>
2016-09-06 13:01:25 +0530

    #NVLI-466 - Curators View Create a form to add media items to play list

---
+++ modules/custom/eminent_admin/src/Form/AddPlayListForm.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\eminent_admin\Form\AddPlayListForm.
+ */
+
+namespace Drupal\eminent_admin\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\node\Entity\Node;
+/**
+ * Landing page form class.
+ */
+class AddPlayListForm extends FormBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'eminent_admin_add_play_list_form';
+  }
+
+  /**
+   * Callback from webform_insightly_menu().
+   *
+   * Form to processes all tpl files.
+   * To configure custom path and rescan the template files.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $media_id = NULL) {
+    $option = $this->eminent_admin_get_content_type('play_list');
+    $form['play_list'] = [
+      '#title' => t('Play List'),
+      '#type' => 'select',
+      '#description' => 'Select the desired Play list',
+      '#options' => $option,
+    ];
+    
+     $form['media_id'] = [
+      '#type' => 'hidden',
+      '#default_value' => $media_id,
+    ];
+    
+    $form['submit'] = [
+    '#type' => 'submit',
+    '#value' => $this->t('Add'),
+    ];
+    
+    return $form;
+  }
+
+  /**
+   * Callback for admin configuration form.
+   *
+   * If a custom path is set, set it into a variable.
+   */
+  public function submitForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
+    $play_list = $form_state->getValue('play_list');
+    $media_id = $form_state->getValue('media_id');
+    $node = Node::load($play_list);
+    $node->field_resource->appendItem($media_id);
+    $node->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'eminent_admine.settings',
+    ];
+  }
+  
+  public function eminent_admin_get_content_type($content_type) {
+    $options['select'] = t('--- SELECT ---');
+    $query = \Drupal::entityQuery('node')
+      ->condition('type', $content_type);
+    $result = $query->execute();
+    if (!empty($result)) {
+      $nodes = node_load_multiple($result);
+      foreach ($nodes as $node) {
+       $options[$node->id()] = $node->getTitle();
+      }
+    }
+    return $options;
+  }
+}
