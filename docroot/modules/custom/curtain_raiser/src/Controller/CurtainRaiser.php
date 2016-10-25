809e757562481f686f76472d023ecf51ae5822c9
Muhammed Mishal <muhammed.mishal@zyxware.com>
2016-10-25 12:23:51 +0530

    Curtain raiser settings form to enter the master password, inauguration status.

--- docroot/modules/custom/curtain_raiser/src/Controller/CurtainRaiser.php
+++ docroot/modules/custom/curtain_raiser/src/Controller/CurtainRaiser.php
@@ -2,18 +2,29 @@
 
 /**
  * @file
- * Contains \Drupal\curtain_raiser\Controller\MediaAdd.
+ * Contains \Drupal\curtain_raiser\Controller\CurtainRaiser.
  */
 
 namespace Drupal\curtain_raiser\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
 
 class CurtainRaiser extends ControllerBase {
 
   public function validate($password) {
-    if ($password == "user0123") {
+
+      $config = \Drupal::service('config.factory')->getEditable('curtain_raiser.settings');
+
+      $master_password = $config->get('master_password');
+
+
+    if ($password == $master_password) {
+
+      $config->set('inauguration_status', true)
+        ->save();
       $response = array(
         'success' => TRUE,
       );
