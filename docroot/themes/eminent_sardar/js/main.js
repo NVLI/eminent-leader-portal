e86c81f3193c78913d7649e3584df9808d8df6a4
Muhammed Mishal <muhammed.mishal@zyxware.com>
2016-11-07 14:42:28 +0530

    Paragraphs in exhibitions and theme update.

--- docroot/themes/eminent_sardar/js/main.js
+++ docroot/themes/eminent_sardar/js/main.js
@@ -7,6 +7,29 @@ jQuery(function($) {'use strict',
     });
   });*/
 
+  $(document).ready(function(){
+
+  	var clickEvent = false;
+  	$('#playlist').carousel({
+  		interval:   4000
+  	}).on('click', '.list-group li', function() {
+  			clickEvent = true;
+  			$('.list-group li').removeClass('active');
+  			$(this).addClass('active');
+  	}).on('slid.bs.carousel', function(e) {
+  		if(!clickEvent) {
+  			var count = $('.list-group').children().length -1;
+  			var current = $('.list-group li.active');
+  			current.removeClass('active').next().addClass('active');
+  			var id = parseInt(current.data('slide-to'));
+  			if(count == id) {
+  				$('.list-group li').first().addClass('active');
+  			}
+  		}
+  		clickEvent = false;
+  	});
+  })
+
 
   // accordian
   $('.accordion-toggle').on('click', function(){
