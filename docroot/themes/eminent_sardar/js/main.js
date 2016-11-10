518419c1280a59fa4635c7dd2d6a1456e71c4805
Muhammed Mishal <muhammed.mishal@zyxware.com>
2016-11-10 17:15:28 +0530

    Coding standard fix for the theme.

--- docroot/themes/eminent_sardar/js/main.js
+++ docroot/themes/eminent_sardar/js/main.js
@@ -1,68 +1,60 @@
-jQuery(function($) {'use strict',
+/**
+ * @file
+ * Custom js files for the theme.
+ */
 
-  //#main-slider
-  /*$(function(){
-    $('#main-slider.carousel').carousel({
-      interval: 8000
-    });
-  });*/
+jQuery(function ($) {'use strict',
 
-  $(document).ready(function(){
+  $(document).ready(function () {
 
-  	var clickEvent = false;
-  	$('#playlist').carousel({
-  		interval:   4000
-  	}).on('click', '.list-group li', function() {
-  			clickEvent = true;
-  			$('.list-group li').removeClass('active');
-  			$(this).addClass('active');
-  	}).on('slid.bs.carousel', function(e) {
-  		if(!clickEvent) {
-  			var count = $('.list-group').children().length -1;
-  			var current = $('.list-group li.active');
-  			current.removeClass('active').next().addClass('active');
-  			var id = parseInt(current.data('slide-to'));
-  			if(count == id) {
-  				$('.list-group li').first().addClass('active');
-  			}
-  		}
-  		clickEvent = false;
-  	});
-  })
+    var clickEvent = false;
+    $('#playlist').carousel({
+      interval:   4000
+      }).on('click', '.list-group li', function () {
+        clickEvent = true;
+        $('.list-group li').removeClass('active');
+        $(this).addClass('active');
+      }).on('slid.bs.carousel', function (e) {
+        if (!clickEvent) {
+          var count = $('.list-group').children().length - 1;
+          var current = $('.list-group li.active');
+          current.removeClass('active').next().addClass('active');
+          var id = parseInt(current.data('slide-to'));
+          if (count == id) {
+            $('.list-group li').first().addClass('active');
+          }
+        }
+        clickEvent = false;
+      });
+  });
 
-
-  // accordian
-  $('.accordion-toggle').on('click', function(){
-  $(this).closest('.panel-group').children().each(function(){
+  // Accordian.
+  $('.accordion-toggle').on('click', function () {
+  $(this).closest('.panel-group').children().each(function () {
   $(this).find('>.panel-heading').removeClass('active');
    });
 
    $(this).closest('.panel-heading').toggleClass('active');
   });
 
-/***********************************************************************
-*******Play video on load.. */
-
+  // Play video on load.
   $(window).on("load", function () {
-    Modernizr.on('videoautoplay', function(result) {
+    Modernizr.on('videoautoplay', function (result) {
       if (result) {
 
-      } else {
+      }
+      else {
         $("#myCarousel").carousel(1);
       }
     });
   });
-  /***********************************************************************
-  *******Hide video after play. */
 
-  $('#bannervideo').on('ended',function(){
+  // Hide video after play.
+  $('#bannervideo').on('ended',function () {
     $("#myCarousel").carousel(1);
   });
 
-
-/***********************************************************************
-*******fix the top bar on scroll*/
-
+  // Fix the top bar on scroll.
   var lastScrollTop = 0;
   $(window).on("scroll", function () {
 
@@ -74,19 +66,15 @@ jQuery(function($) {'use strict',
       else {
         $('#header').fadeOut('slow');
       }
-
       lastScrollTop = st;
     }
   });
 
-/***********************************************************************
-*******Full screen view for the media page*/
-
+  // Full screen view for the media page.
   var fullscreen = false;
   $('.resize-btn').on("click", function () {
-    if(!fullscreen)
-    {
-      $('.media-preview').css( {
+    if (!fullscreen) {
+      $('.media-preview').css({
         'position': 'fixed',
         'top': 0,
         'bottom': 0,
@@ -96,7 +84,7 @@ jQuery(function($) {'use strict',
         'z-index': 9999
       });
 
-      $('.resize-btn').css( {
+      $('.resize-btn').css({
         'position': 'fixed',
         'bottom': 0,
         'right': 0,
@@ -107,7 +95,7 @@ jQuery(function($) {'use strict',
       fullscreen = true;
     }
     else {
-      $('.media-preview').css( {
+      $('.media-preview').css({
         'position': 'relative',
         'margin-bottom': '70px',
         'z-index': 0
@@ -122,11 +110,9 @@ jQuery(function($) {'use strict',
       fullscreen = false;
     }
 
-
   });
 
-/***********************************************************************
-*******fix search show on the responsive*/
+  // Fix search show on the responsive.
   $(window).on("load resize ", function () {
     if ($(window).width() < 768) {
 
@@ -134,18 +120,12 @@ jQuery(function($) {'use strict',
     };
   });
 
-/***********************************************************************
-*******Initialize search i toggle header */
-  /*$('#header i.fa-search').click(function(){
-    $(this).closest('.search').toggleClass('active');
-  });*/
-/***********************************************************************
-*******Initialize slick clone height to */
-   $.fn.cloneheight = function ( addheightto ) {
+  // Initialize slick clone height.
+   $.fn.cloneheight = function (addheightto) {
     var $this = $(this);
-    $this.each(function() {
+    $this.each(function () {
       $(window).on("load resize", function () {
-        $this.imagesLoaded( function() {
+        $this.imagesLoaded(function () {
          if ($(window).width() > 973) {
           var height = $this.height();
           var container = addheightto;
@@ -158,167 +138,108 @@ jQuery(function($) {'use strict',
   };
   $('#main-slider .slider-item img').cloneheight("#main-slider .slider-item");
 
-
-
-/***********************************************************************
-*******Trigger the quiz manualy on scroll*/
-function isVisible($el) {
-  var winTop = $(window).scrollTop();
-  var winBottom = winTop + $(window).height();
-  var elTop = $el.offset().top;
-  var elBottom = elTop + $el.height();
-  return ((elBottom<= winBottom) && (elTop >= winTop));
-}
-$(document).on('scroll', function() {
-  if($('#conatcat-info').length > 0 ) {
-    if( isVisible($("#conatcat-info")) ) {
-        $(document).off('scroll');
-        $('#launchquiz').modal('show');
-    }
+  // Trigger the quiz manualy on scroll.
+  function isVisible($el) {
+    var winTop = $(window).scrollTop();
+    var winBottom = winTop + $(window).height();
+    var elTop = $el.offset().top;
+    var elBottom = elTop + $el.height();
+    return ((elBottom <= winBottom) && (elTop >= winTop));
   }
-});
-/***********************************************************************
-*******Dyanmically distribute height for the slider*/
+  $(document).on('scroll', function () {
+    if ($('#conatcat-info').length > 0) {
+      if (isVisible($("#conatcat-info"))) {
+          $(document).off('scroll');
+          $('#launchquiz').modal('show');
+      }
+    }
+  });
+
+  // Dyanmically distribute height for the slider.
   $(window).on("load resize", function () {
     var cloneHeight = $('.quotes-by-sardar-holder').height();
     $('.share-feedbackholder').css('height', cloneHeight + 'px');
   });
 
-
-
-/***********************************************************************
-*******Dyanmically distribute height for the slider*/
-
-/*  $(window).on("load resize", function () {
-    var winh = $(window).height();
-    var winhper = ($(window).height() * 50)/100;
-    console.log("winh" + winhper)
-    $('#main-slider .slider-item').css('height', winhper + 'px')
-  });
-
-$.fn.centercontent = function () {
-  var $this = $(this);
-  $(window).on("load resize", function () {
-    $this.each(function () {
-      var containerHeight = $this.height();
-      var ChildrenHeight = $this.find('.content-holder').height();
-      console.log(containerHeight);
-      console.log(ChildrenHeight);
-      var margin = (containerHeight - ChildrenHeight) / 2;
-//      console.log(margin);
-      $this.find('.content-holder').css('padding-top', margin + "px");
+  // Initialize slick quiz for the quiz block in the home page.
+  if ($('#slickQuiz').length > 0) {
+    $('#slickQuiz').slickQuiz();
+    $('.startQuiz').click(function () {
+      $('.quizHeader, .quizName').hide();
     });
-  });
-};
-$('#main-slider').centercontent()*/
+  }
 
-/***********************************************************************
-*******Initialize slick quiz for the quiz block in the home page*/
-if ($('#slickQuiz').length > 0) {
-  $('#slickQuiz').slickQuiz();
-  $('.startQuiz').click(function(){
-    $('.quizHeader, .quizName').hide();
-  });
-}
-/***********************************************************************
-*******Added the slider home page to show the quotes of Sardar Patel*/
-$(".quotes-by-sardar").slick({
-   slidesToShow: 1,
+  // Added the slider home page to show the quotes of Sardar Patel.
+  $(".quotes-by-sardar").slick({
+    slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
     speed: 1000,
     touchMove: true,
     dots: true,
     autoplay: false
-});
-/***********************************************************************
-*******initialize the eqaul height on pages*/
+  });
 
-$('.equal_height').matchHeight();
-/***********************************************************************
-*******Added the exibition slider home page to show the lists of excibition*/
-$(".sliderexcibition").slick({
-   slidesToShow: 4,
+  // Initialize the eqaul height on pages.
+  $('.equal_height').matchHeight();
+
+  // Added the exibition slider home page to show the lists of excibition.
+  $(".sliderexcibition").slick({
+    slidesToShow: 4,
     slidesToScroll: 1,
     arrows: false,
     touchMove: true,
     dots: true,
     autoplay: true,
-  responsive: [
-      {
-        breakpoint: 1900,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 1,
-          infinite: false,
-          dots: true
-        }
-      },{
-        breakpoint: 980,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: false,
-          dots: true
-        }
-      },{
-        breakpoint: 400,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: false,
-          dots: true
-        }
+    responsive: [{
+      breakpoint: 1900,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true
       }
-    ]
+    },{
+      breakpoint: 980,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true
+      }
+    },{
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true
+      }
+    }]
+  });
 
-});
-/***********************************************************************
-*******Auto play the home page video*/
-$('.search-results-tab .col-md-4:nth-child(3n+3)').after('<div class="clearfix"></div>');
+  // Auto play the home page video.
+  $('.search-results-tab .col-md-4:nth-child(3n+3)').after('<div class="clearfix"></div>');
 
-/***********************************************************************
-*******Auto play the home page video*/
-/*jQuery( document ).ready(function($) {
-  $('#header .navbar-right .views-exposed-form').addClass('search');
-  $('#header .navbar-right .views-exposed-form form').attr('role', 'form');
-  if ($('#vid').length > 0) {
-    var vid = document.getElementById("vid");
-    function playVid() {
-      vid.play();
-    }
-    playVid();
-  }
-});*/
-/***********************************************************************
-*******Added the slider home page to show the quotes of Sardar Patel*/
-  $(".triggersearchfilter").click(function(){
+  // Added the slider home page to show the quotes of Sardar Patel.
+  $(".triggersearchfilter").click(function () {
     $(this).siblings('.search-filter').toggleClass('hidden show', 1000)
     if ($('.search-filter').hasClass('show')) {
       $(this).html("<i class='fa fa-times-circle'></i> &nbsp; Close Filters");
-    } else {
+    }
+    else {
       $(this).html("<i class='fa fa-filter'></i> &nbsp; Show Filters");
     }
   });
-/***********************************************************************
-*******Added the slider home page to show the quotes of Sardar Patel*/
-$(".quoteslider").slick({
-   slidesToShow: 1,
+  // Added the slider home page to show the quotes of Sardar Patel.
+  $(".quoteslider").slick({
+    slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
     touchMove: true
-});
+  });
 
-
-/*$("#main-slider").slick({
-   slidesToShow: 1,
-    slidesToScroll: 0,
-    arrows: false,
-    touchMove: false
-});*/
-
-/***********************************************************************
-*******Added the slider in the exibition page with  content over slider*/
+  // Added the slider in the exibition page with content over slider.
   $('.exibition_slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -363,88 +284,71 @@ $(".quoteslider").slick({
     ]
   });
 
-
-/***********************************************************************
-*******Vertical center the contents in the hopage slider*/
-
-$.fn.VerticalCenter = function(container) {
+  // Vertical center the contents in the hopage slider.
+  $.fn.VerticalCenter = function (container) {
     var $this = $(this)
     $(window).on("load resize ", function () {
       if ($(window).width() > 973) {
-        $('.slider-item img').imagesLoaded( function() {
-      var element = $this;
-      var containerel = $(container)
-      var elementHeight = $(element).height();
-      var containerHeight = $(container).height();
-      var marginTop =  (containerHeight - elementHeight) / 2;
-      $this.css('margin-top', marginTop + 'px');
+        $('.slider-item img').imagesLoaded(function () {
+          var element = $this;
+          var containerel = $(container)
+          var elementHeight = $(element).height();
+          var containerHeight = $(container).height();
+          var marginTop = (containerHeight - elementHeight) / 2;
+          $this.css('margin-top', marginTop + 'px');
+        });
+      }
     });
-    }
-  });
-};
+  };
 
-$('.slidercontent').VerticalCenter('.slider-item img');
+  $('.slidercontent').VerticalCenter('.slider-item img');
 
-
-/***********************************************************************
-******* send the header aboe the slider in homepage*/
-/*$(window).on("load resize scroll", function () {
-    if ($(window).width() > 768) {
-        var navbarHeight = $('header#header').height();
-        $('#main-slider').css('margin-top', '-' + navbarHeight + 'px');
-    };
-  });*/
-
-
-
-
-  //Initiat WOW JS
+  // Initiat WOW JS.
   new WOW().init();
 
-  // portfolio filter
-  $(window).load(function(){'use strict';
-  var $portfolio_selectors = $('.portfolio-filter >li>a');
-  var $portfolio = $('.portfolio-items');
-  $portfolio.isotope({
-   itemSelector : '.portfolio-item',
-   layoutMode : 'fitRows'
+  // Portfolio filter.
+  $(window).load(function () {'use strict';
+    var $portfolio_selectors = $('.portfolio-filter >li>a');
+    var $portfolio = $('.portfolio-items');
+    $portfolio.isotope({
+     itemSelector : '.portfolio-item',
+     layoutMode : 'fitRows'
+    });
+
+    $portfolio_selectors.on('click', function () {
+      $portfolio_selectors.removeClass('active');
+      $(this).addClass('active');
+      var selector = $(this).attr('data-filter');
+      $portfolio.isotope({ filter: selector });
+      return false;
+    });
   });
 
-  $portfolio_selectors.on('click', function(){
-   $portfolio_selectors.removeClass('active');
-   $(this).addClass('active');
-   var selector = $(this).attr('data-filter');
-   $portfolio.isotope({ filter: selector });
-   return false;
-  });
-  });
-
-  // Contact form
+  // Contact form.
   var form = $('#main-contact-form');
-  form.submit(function(event){
+  form.submit(function (event) {
     event.preventDefault();
     var form_status = $('<div class="form_status"></div>');
     $.ajax({
       url: $(this).attr('action'),
 
-      beforeSend: function(){
-        form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
+      beforeSend: function () {
+        form.prepend(form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn());
       }
-    }).done(function(data){
+    }).done(function (data) {
       form_status.html('<p class="text-success">' + data.message + '</p>').delay(3000).fadeOut();
     });
   });
 
-
-  //goto top
-  $('.gototop').click(function(event) {
+  // Goto top.
+  $('.gototop').click(function (event) {
     event.preventDefault();
     $('html, body').animate({
       scrollTop: $("body").offset().top
     }, 500);
   });
 
-  //Pretty Photo
+  // Pretty Photo.
   $("a[rel^='prettyPhoto']").prettyPhoto({
     social_tools: false
   });
