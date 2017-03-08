9543d8c307cd1c36dd70effe997c4fc62955093f
Muhammed Mishal <muhammed.mishal@zyxware.com>
2017-03-08 14:47:30 +0530

    Theme update for the PDF to image converted images

--- docroot/themes/eminent_sardar/js/main.js
+++ docroot/themes/eminent_sardar/js/main.js
@@ -7,6 +7,91 @@ jQuery(function ($) {'use strict',
 
   $(document).ready(function () {
 
+
+      var console = window.console || { log: function () {} };
+      var $images = $('.docs-pictures');
+      var $toggles = $('.docs-toggles');
+      var $buttons = $('.docs-buttons');
+      var options = {
+            // inline: true,
+            url: 'data-original',
+            title: false,
+            transition: false,
+            scalable: false
+          };
+
+      function toggleButtons(mode) {
+        if (/modal|inline|none/.test(mode)) {
+          $buttons.
+            find('button[data-enable]').
+            prop('disabled', true).
+              filter('[data-enable*="' + mode + '"]').
+              prop('disabled', false);
+        }
+      }
+
+      $images.on({
+        'build.viewer': function (e) {
+          console.log(e.type);
+        },
+        'built.viewer':  function (e) {
+          console.log(e.type);
+        },
+        'show.viewer':  function (e) {
+          console.log(e.type);
+        },
+        'shown.viewer':  function (e) {
+          console.log(e.type);
+        },
+        'hide.viewer':  function (e) {
+          console.log(e.type);
+        },
+        'hidden.viewer': function (e) {
+          console.log(e.type);
+        },
+        'view.viewer':  function (e) {
+          console.log(e.type);
+        },
+        'viewed.viewer': function (e) {
+          console.log(e.type);
+        }
+      }).viewer(options);
+
+      toggleButtons(options.inline ? 'inline' : 'modal');
+
+      $toggles.on('change', 'input', function () {
+        var $input = $(this);
+        var name = $input.attr('name');
+
+        options[name] = name === 'inline' ? $input.data('value') : $input.prop('checked');
+        $images.viewer('destroy').viewer(options);
+        toggleButtons(options.inline ? 'inline' : 'modal');
+      });
+
+      $buttons.on('click', 'button', function () {
+        var data = $(this).data();
+        var args = data.arguments || [];
+
+        if (data.method) {
+          if (data.target) {
+            $images.viewer(data.method, $(data.target).val());
+          } else {
+            $images.viewer(data.method, args[0], args[1]);
+          }
+
+          switch (data.method) {
+            case 'scaleX':
+            case 'scaleY':
+              args[0] = -args[0];
+              break;
+
+            case 'destroy':
+              toggleButtons('none');
+              break;
+          }
+        }
+      });
+
     var clickEvent = false;
     $('#playlist').carousel({
       interval:   4000
