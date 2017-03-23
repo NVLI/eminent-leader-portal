5343fc0a83e6548ba708b82a0052dc0c7d1dee9a
Jimmy Sebastian <jimmy.sebastian@zyxware.com>
2017-03-23 15:28:01 +0530

    #NVLI-599-watermark-images
    Correction in backup process in before apply watermark.

--- docroot/modules/custom/eminent_watermark/src/Controller/VideoWatermark.php
+++ docroot/modules/custom/eminent_watermark/src/Controller/VideoWatermark.php
@@ -54,46 +54,48 @@ class VideoWatermark extends ControllerBase {
 	  $nodes = $node_storage->loadMultiple($nodeIds);
       if (!empty($nodes)) {
         foreach ($nodes as $node) {
-		  $file_referance = $node->field_media_video->getValue();
-		  if (!empty($file_referance)) {
-		    $fid = $file_referance[0]['target_id'];
-            $file_storage = $this->entity_manager->getStorage('file');
-		    $file = $file_storage->load($fid);
-		    if (!empty($file->getFileUri())) {
-			  $files[] = $file->getFileUri();
+					$file_referance = $node->field_media_video->getValue();
+					if (!empty($file_referance)) {
+						$fid = $file_referance[0]['target_id'];
+								$file_storage = $this->entity_manager->getStorage('file');
+						$file = $file_storage->load($fid);
+						if (!empty($file->getFileUri())) {
+							$files[] = $file->getFileUri();
+						}
+					}
+				}
+				$file_names = fopen("public://archive/watermark-video-name.txt", "a+") or die("Unable to open file!");
+				// Output one character until end-of-file
+				$video_names = array();
+				while(!feof($file_names)) {
+					$videos[] = trim(fgets($file_names));
+				}
+				fclose($file_names);
+				// Create watermark
+				$supporting_format = array('mpg', 'mpge', 'mp4');
+				foreach ($files as $file) {
+					$file_name = basename($file);
+					if (!in_array($file_name, $videos)) {
+						$ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+						if (in_array($ext, $supporting_format)) {
+							$status = $this->create_watermark($file, $file_name);
+							if ($status == 1) {
+								$fh = fopen("public://archive/watermark-video-name.txt", "a+") or die("Unable to open file!");
+								fwrite($fh, $file_name . "\n");
+								fclose($fh);
+							}
+						}
+					}
+				}
+				$element = array(
+					'#markup' => 'Watermark Created Successfully ',
+				);
+				return $element;
 			}
-		  }
-		}
-		$file_names = fopen("public://archive/watermark-video-name.txt", "a+") or die("Unable to open file!");
-		// Output one character until end-of-file
-		$video_names = array();
-		while(!feof($file_names)) {
-		  $videos[] = trim(fgets($file_names));
-		}
-		fclose($file_names);
-		// Create watermark
-		$supporting_format = array('mpg', 'mpge', 'mp4');
-		foreach ($files as $file) {
-          $file_name = basename($file);
-          if (!in_array($file_name, $videos)) {
-            $ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
-			if (in_array($ext, $supporting_format)) {
-              $status = $this->create_watermark($file, $file_name);
-			  $fh = fopen("public://archive/watermark-video-name.txt", "a+") or die("Unable to open file!");
-              fwrite($fh, $file_name . "\n");
-			  fclose($fh);
-			}
-		  }
-		}
-		$element = array(
-		  '#markup' => 'Watermark Created Successfully ',
-		);
-        return $element;
-	  }
     }
-	$element = array(
-		'#markup' => 'No Videos founds. ',
-	);
+		$element = array(
+			'#markup' => 'No Videos founds. ',
+		);
     return $element;
   }
 
@@ -102,29 +104,38 @@ class VideoWatermark extends ControllerBase {
    */
   public function create_watermark($uri, $file_name) {
     // Copy orignal file to another folder before create watermark.
-	$public_path_backup = 'public://archive/video-backup/';
-	file_prepare_directory($public_path_backup, FILE_CREATE_DIRECTORY);
-	$original_video = file_get_contents($uri);
-	$file_name = basename($uri);
-	if ($original_video) {
-	  $save_file = file_save_data($original_video, $public_path_backup . $file_name, FILE_EXISTS_RENAME);
-	}
-	$ffmpeg = \FFMpeg\FFMpeg::create([
-		'ffmpeg.binaries'  => exec('which ffmpeg'),
-		'ffprobe.binaries' => exec('which ffprobe'),
-		'timeout'          => 3600, // the timeout for the underlying process
-		'ffmpeg.threads'   => 1,   // the number of threads that FFMpeg should use
-	], $logger);
-	$video = $ffmpeg->open($uri);
-	$video
-		->filters()
-		->watermark('sites/default/files/video_watermark.png', array(
-			'position' => 'relative',
-			'bottom' => 50,
-			'right' => 50,
-		));
-	$video
-       ->save(new \FFMpeg\Format\Video\WebM(),  $uri);
-	return 1;
+		$uri = drupal_realpath($uri);
+		$public_path_backup = drupal_realpath('public://archive/video-backup');
+		file_prepare_directory($public_path_backup, FILE_CREATE_DIRECTORY);
+		$file_name = basename($uri);
+		$isFileExists =  file_exists($uri);
+		$destination_path = $public_path_backup . "/" . $file_name;
+    $isBackupFileExist =  file_exists($destination_path);
+		if ($isFileExists && $isBackupFileExist != 1) {
+			if (!copy($uri, $destination_path )) {
+					return 0;
+			}
+			$ffmpeg = \FFMpeg\FFMpeg::create([
+				'ffmpeg.binaries'  => exec('which ffmpeg'),
+				'ffprobe.binaries' => exec('which ffprobe'),
+				'timeout'          => 3600, // the timeout for the underlying process
+				'ffmpeg.threads'   => 1,   // the number of threads that FFMpeg should use
+			], $logger);
+			$video = $ffmpeg->open($uri);
+			$video
+				->filters()
+				->watermark('sites/default/files/video_watermark.png', array(
+					'position' => 'relative',
+					'bottom' => 50,
+					'right' => 50,
+				));
+			$video
+					 ->save(new \FFMpeg\Format\Video\WebM(),  $uri);
+			return 1;
+		}
+		else {
+			return 0;
+		}
+		return 0;
   }
 }
